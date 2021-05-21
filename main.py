8f47a187f3fa958819f34a1151df6408431d34ff
Shanu Jha <shanu.j@foxberry.in>
2021-05-21 22:05:08 +0530

    Configure the basic settings package

---
+++ main.py
@@ -0,0 +1,6 @@
+from settings import config
+print(config.consumer_key)
+print(config.consumer_secret)
+print(config.access_token)
+print(config.access_token_secret)
+print(config.bearer_token)
---
+++ settings/config.py
@@ -0,0 +1,14 @@
+# settings.py
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+# Import twitter api keys from the .env file.
+consumer_key = os.environ.get('API_KEY')
+consumer_secret = os.environ.get('API_SECRET')
+access_token = os.environ.get('ACCESS_TOKEN')
+access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
+bearer_token = os.environ.get('BEARER_TOKEN')
